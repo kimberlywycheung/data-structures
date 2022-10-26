76319c678673fb9823da1ad3488af4e58f381224
James Leakos <jamesleakos@gmail.com>
2022-10-26 13:31:46 -0700

    implement prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,21 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.storage = [];
+  return instance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function (value) {
+  this.storage.unshift(value);
+};
+
+queueMethods.dequeue = function () {
+  return this.storage.pop();
+};
+
+queueMethods.size = function () {
+  return this.storage.length;
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,9 +1,34 @@
 var Stack = function() {
-  var instance = Object.create(Stack.prototype);
+  var instance = Object.create(stackMethods);
+  instance.storage = [];
+  return instance;
 };
 
-Stack.prototype.pop = function () {
-  return this.
+var stackMethods = {};
+
+// Stack.prototype.pop = function () {
+//   return this.storage.pop();
+// };
+
+// Stack.prototype.push = function (value) {
+//   this.storage.push(value);
+// };
+
+// Stack.prototype.size = function () {
+//   return this.storage.length;
+// };
+
+stackMethods.pop = function () {
+  return this.storage.pop();
 };
+
+stackMethods.push = function (value) {
+  this.storage.push(value);
+};
+
+stackMethods.size = function () {
+  return this.storage.length;
+};
+
 
 
