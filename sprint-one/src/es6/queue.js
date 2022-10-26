f060b1986c8e7cd10b34bb8a2bfdb5310859d9af
James Leakos <jamesleakos@gmail.com>
2022-10-26 13:57:43 -0700

    complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,19 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = [];
+  }
+
+  enqueue(value) {
+    this.storage.unshift(value);
+  }
+
+  dequeue() {
+    return this.storage.pop();
+  }
+
+  size() {
+    return this.storage.length;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,18 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = [];
   }
 
+  push(value) {
+    this.storage.push(value);
+  }
+
+  pop() {
+    return this.storage.pop();
+  }
+
+  size() {
+    return this.storage.length;
+  }
 }
