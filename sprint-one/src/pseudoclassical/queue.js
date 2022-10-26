4e4945e1c80ce940d6c60f3f311334de1f7cac6e
James Leakos <jamesleakos@gmail.com>
2022-10-26 13:45:31 -0700

    implement pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,15 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = [];
 };
 
+Queue.prototype.enqueue = function (value) {
+  this.storage.unshift(value);
+};
 
+Queue.prototype.dequeue = function () {
+  return this.storage.pop();
+};
+
+Queue.prototype.size = function () {
+  return this.storage.length;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,19 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = [];
 };
+
+Stack.prototype.push = function (value) {
+  this.storage.push(value);
+};
+
+Stack.prototype.pop = function () {
+  return this.storage.pop();
+};
+
+Stack.prototype.size = function () {
+  return this.storage.length;
+};
+
+
 
 
