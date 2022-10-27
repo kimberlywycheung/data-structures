031aa44c6b4cef4b8f0d35e32413e11358af39ec
Kimberly Cheung <kimberlywycheung@gmail.com>
2022-10-27 10:09:09 -0700

    tree additions

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,40 @@
-var Tree = function(value) {
-  var newTree = {};
-  newTree.value = value;
+// note: todo: refactor away from es6, spec does not allow for this one
 
-  // your code here
-  newTree.children = null;  // fix me
+class Tree {
+  constructor(value) {
+    this.value = value;
+    this.children = [];
+  }
 
-  return newTree;
-};
+  addChild (value) {
+    // make new tree with value
+    var newTree = new Tree(value);
+    // add to this.children
+    this.children.push(newTree);
+  }
 
-var treeMethods = {};
+  contains(target) {
+    var searchTree = function (tree) {
+      if (tree.value === target) {
+        return true;
+      } else {
+        if (tree.children.length > 0) {
+          for (var i = 0; i < tree.children.length; i++) {
+            if (searchTree(tree.children[i])) {
+              return true;
+            }
+          }
+          return false;
+        } else {
+          return false;
+        }
+      }
+    };
+  }
 
-treeMethods.addChild = function(value) {
-};
 
-treeMethods.contains = function(target) {
-};
 
+}
 
 
 /*
